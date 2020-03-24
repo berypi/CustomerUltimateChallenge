3f439d4f454907fa663c20854dc62bd13ee1de7d
gdc <garret.dechavez@gmail.com>
2020-03-24 18:20:18 -0500

    added partial and css to partials

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root to: "customers#index"
   get "customers/alphabetized", to: "customers#alphabetized"
   get "customers/missing_email", to: "customers#missing_email"
+  # resources "customers", only: %i[alphabetized missing_email]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
